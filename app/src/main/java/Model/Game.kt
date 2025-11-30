f4530f030c16e19475dfe8ae1214b6d5b4d06de9
EI-Zolana <erfrancisco268@gmail.com>
2025-11-30 11:40:01 +0000

    created data classe for game

--- app/src/main/java/Model/Game.kt
+++ app/src/main/java/Model/Game.kt
@@ -1,2 +1,16 @@
 package Model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+// data classe game para armazenar os dados
+data class Game(
+//Não vou mudar estes valores
+    //usei val para declarar os valores que não quero alterar
+    val id: String,
+    val title: String,
+    val subtitle: String,
+    val description: String,
+    val item: GameItem
+): Parcelable
